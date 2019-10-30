007bc9ec8d20365cfa79fa63a1a9c795fc570d80
sidd <siddtheshah@github.com>
2019-10-30 00:45:04 -0400

    changed downloader to wget

--- data_processing/collection/download_dataset.py
+++ data_processing/collection/download_dataset.py
@@ -1,7 +1,7 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
 import pandas as pd
-import urllib
+import wget
 import os
 
 IMAGE_FOLDER = "/tmp/movie_poster_images"
@@ -10,4 +10,4 @@ frame = pd.read_csv('MovieGenre.csv', encoding="ISO-8859-1")
 
 for movieId, url in zip(frame['imdbId'], frame['Poster']):
     # print(os.path.join(IMAGE_FOLDER, str(movieId) + ".jpg"))
-    urllib.urlretrieve(url, os.path.join(IMAGE_FOLDER, str(movieId) + ".jpg"))
+    wget.download(url, os.path.join(IMAGE_FOLDER, str(movieId) + ".jpg"))
