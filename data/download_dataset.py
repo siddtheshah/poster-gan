830eb5188051d2e60374b358cb007d929e67b2c2
sidd <siddtheshah@github.com>
2019-11-02 22:53:36 -0400

    Added image resizing

---
+++ data/download_dataset.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+import pandas as pd
+import wget
+import os
+from PIL import Image
+
+IMAGE_FOLDER = "movie_poster_images"
+if not os.path.exists(IMAGE_FOLDER):
+    os.makedirs(IMAGE_FOLDER)
+
+frame = pd.read_csv('MovieGenre.csv', encoding="ISO-8859-1")
+
+errors = []
+
+for movieId, url in zip(frame['imdbId'], frame['Poster']):
+    try:
+        image_path = os.path.join(IMAGE_FOLDER, str(movieId) + ".jpg")
+        wget.download(url, image_path)
+        img = Image.open(image_path)
+        img = img.resize((256, 256), Image.ANTIALIAS)
+        img.save(image_path)
+    except:
+        errors.append(movieId)
+
+print("Errors: ", errors)
