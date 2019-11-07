Removed unused dependencies, fixed normalization

tfgan_quick_train_lib.py
@@ -1,5 +1,4 @@
 import tensorflow.compat.v1 as tf_v1
-import cv2
 
 # Allow matplotlib images to render immediately.
 tf_v1.logging.set_verbosity(tf_v1.logging.ERROR)  # Disable noisy outputs.
@@ -64,8 +63,8 @@ def unconditional_generator(noise, mode=True, weight_decay=2.5e-5):
     net = _deconv2d(net, 3, 4.0, 2)
     net = tf_v1.tanh(net)
 
-    net = cv2.normalize(net, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
-    net.astype(np.uint8)
+    net = 127.5*(net+1)
+    #net = tf_v1.dtypes.cast(net, dtype=tf_v1.uint8)
 
     print("Generator shape", net.shape)
     print("Generator created")

tfgan_quick_train_main.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import logging
 import os
-from PIL import Image
 import time
 
 #TODO: set up argparsing.
@@ -15,7 +14,7 @@ import time
 IMAGE_DIR = "/mnt/disks/new_space/movie_poster_images"
 
 RESULTS_DIR = "/mnt/disks/new_space/results"
-RUN_NAME = "first"
+RUN_NAME = "11_6_2019_1"
 
 RUN_DIR = os.path.join(os.getcwd(), RESULTS_DIR, RUN_NAME)
 MODEL_DIR = os.path.join("/mnt/disks/new_space/model", RUN_NAME)
@@ -143,9 +142,9 @@ while cur_step < MAX_TRAIN_STEPS:
     row2 = np.hstack(imgs[5:10])
     row3 = np.hstack(imgs[10:15])
     row4 = np.hstack(imgs[15:])
-    all = np.vstack((row1, row2, row3, row4))
-
-    plt.imshow(all)
+    all_images = np.vstack((row1, row2, row3, row4))
+    all_images = all_images.astype(np.uint8)
+    plt.imshow(all_images)
     plt.savefig(os.path.join(RUN_DIR, "iter" + str(cur_step) + ".png"))
 
 