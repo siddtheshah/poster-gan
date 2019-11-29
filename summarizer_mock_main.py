4e4b0af15660b22deeac59f1920481e65ce66318
Sidd <sidd_shah95@yahoo.com>
2019-11-29 11:28:59 -0500

    Removed path args in favor of config

--- summarizer_mock_main.py
+++ summarizer_mock_main.py
@@ -17,9 +17,6 @@ import argparse
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--train', default=False, action='store_true', help='Train a new model')
 parser.add_argument('--eval', default=False, action='store_true', help='Run eval on the model')
-parser.add_argument('--storage_dir', default='/mnt/disks/new_space/ml/summarizer', action='store', help='Specify path of results and model data')
-parser.add_argument('--trailer_dir', default='/mnt/disks/new_space/ml_trailers_key_frame_features', action='store', help='Specify poster data path')
-parser.add_argument('--poster_dir', default='/mnt/disks/new_space/movie_poster_images', action='store', help='Specify poster data path')
 parser.add_argument('--run_name', help='Specify a run name. (Required)', action='store')
 args = parser.parse_args()
 
@@ -30,14 +27,14 @@ def test_gan_io(configs):
     generator = MockGenerator()
     generator._set_inputs(tf_v1.keras.Input(shape=(64.0, 64.0, 3.0)))
     generator.compile(loss='mse', optimizer='rmsprop')
-    generator.save(os.path.join(args.storage_dir, "generator"), save_format='tf')
-    generator = tf_v1.keras.models.load_model(os.path.join(args.storage_dir, "generator"))
+    generator.save(os.path.join(configs["storage_dir"], "generator"), save_format='tf')
+    generator = tf_v1.keras.models.load_model(os.path.join(configs["storage_dir"], "generator"))
 
     discriminator = MockDiscriminator()
     discriminator._set_inputs(tf_v1.keras.Input(shape=(64.0, 64.0, 3.0)))
     discriminator.compile(loss='mse', optimizer='rmsprop')
-    discriminator.save(os.path.join(args.storage_dir, "discriminator"), save_format='tf')
-    discriminator = tf_v1.keras.models.load_model(os.path.join(args.storage_dir, "discriminator"))
+    discriminator.save(os.path.join(configs["storage_dir"], "discriminator"), save_format='tf')
+    discriminator = tf_v1.keras.models.load_model(os.path.join(configs["storage_dir"], "discriminator"))
 
     z = np.zeros((20, 64, 64, 3))
     z.astype(np.float32)
@@ -51,16 +48,16 @@ def train_new_model(configs):
     generator = MockGenerator()
     generator._set_inputs(tf_v1.keras.Input(shape=(64, 64, 3)))
     generator.compile(loss='mse', optimizer='rmsprop')
-    generator.save(os.path.join(args.storage_dir, "generator"), save_format='tf')
+    generator.save(os.path.join(configs["storage_dir"], "generator"), save_format='tf')
     discriminator = MockDiscriminator()
     discriminator._set_inputs(tf_v1.keras.Input(shape=(64, 64, 3)))
     discriminator.compile(loss='mse', optimizer='rmsprop')
-    discriminator.save(os.path.join(args.storage_dir, "discriminator"), save_format='tf')
+    discriminator.save(os.path.join(configs["storage_dir"], "discriminator"), save_format='tf')
 
     # Compose the training step
-    generator = tf_v2.saved_model.load(export_dir=os.path.join(args.storage_dir, "generator"))
+    generator = tf_v2.saved_model.load(export_dir=os.path.join(configs["storage_dir"], "generator"))
     generator_predict = generator.signatures["serving_default"]
-    discriminator = tf_v2.saved_model.load(export_dir=os.path.join(args.storage_dir, "discriminator"))
+    discriminator = tf_v2.saved_model.load(export_dir=os.path.join(configs["storage_dir"], "discriminator"))
     discriminator_predict = discriminator.signatures["serving_default"]
     print(list(discriminator.signatures.keys()))
 
@@ -68,13 +65,13 @@ def train_new_model(configs):
     beta = configs["beta"]
     batch_size = configs["batch_size"]
 
-    run_dir = os.path.join(args.storage_dir, args.run_name)
+    run_dir = os.path.join(configs["storage_dir"], args.run_name)
     save_dir = os.path.join(run_dir, "model")
 
     with summary_graph.as_default():
         with tf_v1.Session(graph=summary_graph) as sess:
             train_dataset, validation_dataset = \
-                summarizer.dataset.create_summary_dataset(args.trailer_dir, args.poster_dir, batch_size)
+                summarizer.dataset.create_summary_dataset(configs["trailer_dir"], configs["poster_dir"], batch_size)
             model = SummaryNetwork(configs["weight_decay"])
             summarizer_loss = summarizer.eval.summarizer_loss(beta, generator_predict)
             discriminator_loss = summarizer.eval.discriminator_loss(alpha, discriminator_predict, generator_predict)
@@ -88,22 +85,22 @@ def train_new_model(configs):
             callbacks_list = [checkpoint]
             model.fit(train_dataset, epochs=configs["epochs"], verbose=2,
                       validation_data=validation_dataset, callbacks=callbacks_list)
-            model.save(os.path.join(args.storage_dir, args.run_name, "model"), save_format='tf')
+            model.save(os.path.join(configs["storage_dir"], args.run_name, "model"), save_format='tf')
             print("Model finished training.")
 
 def eval_model(configs):
-    run_dir = os.path.join(args.storage_dir, args.run_name)
+    run_dir = os.path.join(configs["storage_dir"], args.run_name)
     results_dir = os.path.join(run_dir, "results")
     save_dir = os.path.join(run_dir, "model")
     model = tf_v2.saved_model.load(save_dir)
 
-    generator = tf_v2.saved_model.load(export_dir=os.path.join(args.storage_dir, "generator"))
+    generator = tf_v2.saved_model.load(export_dir=os.path.join(configs["storage_dir"], "generator"))
 
     # Show real poster vs predictions
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
     summarizer.eval.show_poster_predict_comparison(model, generator,
-                                                   configs["eval_ids"], results_dir, args.poster_dir, args.trailer_dir)
+                                                   configs["eval_ids"], results_dir, configs["poster_dir"], configs["trailer_dir"])
 
     # Other evaluation metrics
 
