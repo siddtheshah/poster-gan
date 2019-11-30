7f3f20996754635df2e0d0a12492f3ff12d00579
Sidd <sidd_shah95@yahoo.com>
2019-11-30 17:22:27 -0500

    Consolidated summarizer main

--- summarizer_main.py
+++ summarizer_main.py
@@ -1,6 +1,8 @@
 import summarizer
 import summarizer.graph
 import summarizer.mock_gan
+from summarizer.mock_gan import MockDiscriminator
+from summarizer.mock_gan import MockGenerator
 import summarizer.eval
 from summarizer.network import SummaryNetwork
 
@@ -12,23 +14,62 @@ import os
 import json
 import argparse
 
-# Make sure you set all the extra options in summarizer_config.json before running this script
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--train', default=False, action='store_true', help='Train a new model')
 parser.add_argument('--eval', default=False, action='store_true', help='Run eval on the model')
 parser.add_argument('--run_name', help='Specify a run name. (Required)', action='store')
+parser.add_argument('--mock', help='Run the summarizer with a mock generator/discriminator', action='store_true',
+                    default=False)
 args = parser.parse_args()
 
 global summary_graph
 summary_graph = summarizer.graph.summary_graph
 
+def test_gan_io(configs):
+    generator = MockGenerator()
+    generator._set_inputs(tf_v1.keras.Input(shape=(64.0, 64.0, 3.0)))
+    generator.compile(loss='mse', optimizer='rmsprop')
+    generator.save(os.path.join(configs["storage_dir"], "generator"), save_format='tf')
+    generator = tf_v1.keras.models.load_model(os.path.join(configs["storage_dir"], "generator"))
+
+    discriminator = MockDiscriminator()
+    discriminator._set_inputs(tf_v1.keras.Input(shape=(64.0, 64.0, 3.0)))
+    discriminator.compile(loss='mse', optimizer='rmsprop')
+    discriminator.save(os.path.join(configs["storage_dir"], "discriminator"), save_format='tf')
+    discriminator = tf_v1.keras.models.load_model(os.path.join(configs["storage_dir"], "discriminator"))
+
+    z = np.zeros((20, 64, 64, 3))
+    z.astype(np.float32)
+    res = generator.predict(z, steps=1)
+    print(res)
+    res = discriminator.predict(z, z, steps=1)
+    print(res)
+
 def train_new_model(configs):
+    # Create and save a mock generator/discriminator
+
+    discriminator_path = configs["discriminator_path"]
+    generator_path = configs["generator_path"]
+
+    if args.mock:
+        generator_path = os.path.join(generator_path, "mock")
+        discriminator_path = os.path.join(discriminator_path, "mock")
+
+        generator = MockGenerator()
+        generator._set_inputs(tf_v1.keras.Input(shape=(64, 64, 3)))
+        generator.compile(loss='mse', optimizer='rmsprop')
+        generator.save(generator_path, save_format='tf')
+        discriminator = MockDiscriminator()
+        discriminator._set_inputs(tf_v1.keras.Input(shape=(64, 64, 3)))
+        discriminator.compile(loss='mse', optimizer='rmsprop')
+        discriminator.save(discriminator_path, save_format='tf')
 
     # Compose the training step
-    generator = tf_v2.saved_model.load(export_dir=configs["generator_path"])
+    generator = tf_v2.saved_model.load(export_dir=generator_path)
     generator_predict = generator.signatures["serving_default"]
-    discriminator = tf_v2.saved_model.load(export_dir=configs["discriminator_path"])
+    discriminator = tf_v2.saved_model.load(export_dir=discriminator_path)
     discriminator_predict = discriminator.signatures["serving_default"]
+    print(list(discriminator.signatures.keys()))
 
     alpha = configs["alpha"]
     beta = configs["beta"]
@@ -44,10 +85,10 @@ def train_new_model(configs):
             model = SummaryNetwork(configs["weight_decay"])
             summarizer_loss = summarizer.eval.summarizer_loss(beta, generator_predict)
             discriminator_loss = summarizer.eval.discriminator_loss(alpha, discriminator_predict, generator_predict)
-            combined_loss = summarizer.eval.combined_loss(alpha, beta, generator_predict, discriminator_predict)
-            model.compile(optimizer=tf_v1.keras.optimizers.Adam(), loss=combined_loss,
-                          metrics=[summarizer_loss, discriminator_loss])
-
+            color_loss = summarizer.eval.color_loss(.5, 8)
+            combined_loss = summarizer.eval.combined_loss(alpha, beta, .5, generator_predict, discriminator_predict, 8)
+            model.compile(optimizer=tf_v1.keras.optimizers.Adam(),
+                          loss=combined_loss, metrics=[summarizer_loss, color_loss])
             # model.compile(optimizer=tf_v1.keras.optimizers.RMSprop(), loss=discriminator_loss, metrics=[])
             # model.compile(optimizer=tf_v1.keras.optimizers.RMSprop(), loss=tf_v1.keras.losses.mean_squared_error, metrics=[])
             checkpoint = tf_v1.keras.callbacks.ModelCheckpoint(os.path.join(save_dir, "model"), monitor='val_loss',
@@ -69,20 +110,20 @@ def eval_model(configs):
     # Show real poster vs predictions
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
-    summarizer.eval.show_poster_predict_comparison(model, generator, results_dir, configs["trailer_dir"],
-                                                   configs["poster_dir"])
+    summarizer.eval.show_poster_predict_comparison(model, generator, results_dir, configs["trailer_dir"], configs["poster_dir"])
 
     # Other evaluation metrics
-
 
 def main():
     with open('summarizer_config.json') as config_file:
         configs = json.load(config_file)
+        # test_gan_io_2(configs)
+
     if args.train:
         train_new_model(configs)
+
     if args.eval:
         eval_model(configs)
-
 
 if __name__ == "__main__":
     main()
