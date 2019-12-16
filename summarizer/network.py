99a210f5720179ced138d9535dc0dee99c0425e4
sidd <siddtheshah@github.com>
2019-12-16 00:38:43 -0500

    Refactored summarizer model for combined training

--- summarizer/network.py
+++ summarizer/network.py
@@ -5,50 +5,95 @@ import summarizer.graph
 global summary_graph
 summary_graph = summarizer.graph.summary_graph
 
+# tf_v1.disable_eager_execution()
+
+def make_mock_cap():
+
+    pad = tf_v1.keras.layers.ZeroPadding2D(padding=(1, 1))
+    def mock_cap(x):
+
+        x = 127.5 * (x + 1)  # Get back on 0-255 scale
+        x = tf_v1.reshape(x, [-1, 62, 62, 3])  # Keras Conv3D doesn't allow directional 'same' padding for kernels
+        # x = tf_v1.Print(x, [x])
+
+        x = pad(x)
+        return x
+    return mock_cap
+
+def make_gan_cap(generator_predict):
+    def call_generator(input):
+        # return tf_v1.zeros([input.shape[0], 64, 64, 3])
+        return generator_predict(input)["generator_output"]
+
+    def gan_cap(x):
+        print(x.shape)
+        # x = self.generator_predict(x)["generator_output"]
+
+        x = tf_v1.py_function(call_generator, [x], tf_v1.float32) # This works, but makes model unsaveable
+        # x = self.generator_predict(x)["generator_output"]
+
+        x = tf_v1.reshape(x, [-1, 64, 64, 3])
+        return x
+
+    return gan_cap
 
 class SummaryNetwork(tf_v1.keras.Model):
-    def __init__(self, weight_decay):
+    def __init__(self, weight_decay, batch_size, filters=40, z_dim=None):
         super(SummaryNetwork, self).__init__()
+        self.batch_size = batch_size
+        self.z_dim = z_dim
         # Define network layers
         # Using Keras's ConvLSTM2D to utilize the temporal dimension of a trailer in a basic way.
 
-        self.cl_1 = tf_v1.keras.layers.ConvLSTM2D(filters=40, strides=1, kernel_size=(3, 3),
+        self.cl_1 = tf_v1.keras.layers.ConvLSTM2D(filters=filters, strides=1, kernel_size=(3, 3),
                                                   padding='same', return_sequences=True,
                                                   # kernel_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                                   # bias_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                                   kernel_initializer='random_uniform',
                                                   bias_initializer='zeros')
         self.bn_1 = tf_v1.keras.layers.BatchNormalization()
-        self.cl_2 = tf_v1.keras.layers.ConvLSTM2D(filters=40, strides=1, kernel_size=(3, 3),
+        self.cl_2 = tf_v1.keras.layers.ConvLSTM2D(filters=filters, strides=1, kernel_size=(3, 3),
                                                   padding='same', return_sequences=True,
                                                   # kernel_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                                   # bias_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                                   kernel_initializer='random_uniform',
                                                   bias_initializer='zeros')
         self.bn_2 = tf_v1.keras.layers.BatchNormalization()
-        self.cl_3 = tf_v1.keras.layers.ConvLSTM2D(filters=40, strides=1, kernel_size=(3, 3),
+        self.cl_3 = tf_v1.keras.layers.ConvLSTM2D(filters=filters, strides=1, kernel_size=(3, 3),
                                                   padding='same', return_sequences=True,
                                                   # kernel_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                                   # bias_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                                   kernel_initializer='random_uniform',
                                                   bias_initializer='zeros')
         self.bn_3 = tf_v1.keras.layers.BatchNormalization()
-        self.cl_4 = tf_v1.keras.layers.ConvLSTM2D(filters=40, strides=1, kernel_size=(3, 3),
+        self.cl_4 = tf_v1.keras.layers.ConvLSTM2D(filters=filters, strides=1, kernel_size=(3, 3),
                                                   padding='same', return_sequences=True,
                                                   # kernel_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                                   # bias_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                                   kernel_initializer='random_uniform',
                                                   bias_initializer='zeros')
         self.bn_4 = tf_v1.keras.layers.BatchNormalization()
+        # Layer for combined model
+
+
+
+        self.pad = tf_v1.keras.layers.ZeroPadding2D(padding=(1, 1))
+
         self.conv = tf_v1.keras.layers.Conv3D(filters=3, strides=1, kernel_size=(20, 3, 3),
                                               kernel_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                               padding='valid', activation='tanh',
                                               bias_regularizer=tf_v1.keras.regularizers.l2(weight_decay),
                                               kernel_initializer='random_uniform',
                                               bias_initializer='zeros')
-        self.pad = tf_v1.keras.layers.ZeroPadding2D(padding=(1, 1))
+        if self.z_dim:
+            self.ln_1 = tf_v1.keras.layers.Dense(z_dim, activation='tanh')
+
+    def call_generator(self, input):
+        # return tf_v1.zeros([input.shape[0], 64, 64, 3])
+        return self.generator_predict(input)["generator_output"]
 
     def call(self, inputs):
+        print(inputs.shape)
         x = self.cl_1(inputs)
         x = self.bn_1(x)
         x = self.cl_2(x)
@@ -57,9 +102,11 @@ class SummaryNetwork(tf_v1.keras.Model):
         x = self.bn_3(x)
         x = self.cl_4(x)
         x = self.bn_4(x)
+        print(x.shape)
         x = self.conv(x)
-        x = 127.5*(x + 1)  # Get back on 0-255 scale
-        x = tf_v1.reshape(x, [-1, 62, 62, 3])  # Keras Conv3D doesn't allow directional 'same' padding for kernels
-        # x = tf_v1.Print(x, [x])
-        x = self.pad(x)
+        print(x.shape)
+        if self.z_dim:
+            x = tf_v1.reshape(x, [-1, 62 * 62 * 3])
+            x = self.ln_1(x)
+
         return x
--- summarizer_main.py
+++ summarizer_main.py
@@ -25,6 +25,8 @@ args = parser.parse_args()
 global summary_graph
 summary_graph = summarizer.graph.summary_graph
 
+# tf_v1.enable_eager_execution()
+
 def test_gan_io(configs):
     generator = MockGenerator()
     generator._set_inputs(tf_v1.keras.Input(shape=(64.0, 64.0, 3.0)))
@@ -64,16 +66,11 @@ def train_new_model(configs):
 #        discriminator.compile(loss='mse', optimizer='rmsprop')
         discriminator.save(discriminator_path, save_format='tf')
 
-    # Compose the training step
-    generator = tf_v2.saved_model.load(export_dir=generator_path)
-    generator_predict = generator.signatures["serving_default"]
-    discriminator = tf_v2.saved_model.load(export_dir=discriminator_path)
-    discriminator_predict = discriminator.signatures["serving_default"]
+
 
     alpha = configs["alpha"]
     beta = configs["beta"]
     gamma = configs["gamma"]
-    batch_size = configs["batch_size"]
 
     run_dir = os.path.join(configs["storage_dir"], args.run_name)
     save_dir = os.path.join(run_dir, "model")
@@ -81,55 +78,81 @@ def train_new_model(configs):
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
+    generator = tf_v2.saved_model.load(export_dir=generator_path)
+    generator_predict = generator.signatures["serving_default"]
+
     with summary_graph.as_default():
         with tf_v1.Session(graph=summary_graph) as sess:
+            # Compose the training step
+
+            discriminator = tf_v2.saved_model.load(export_dir=discriminator_path)
+            discriminator_predict = discriminator.signatures["serving_default"]
+
+            init = tf_v1.global_variables_initializer()
+            sess.run(init) # Initialize the models loaded above.
+
+            if args.mock:
+                summarizer_model = SummaryNetwork(configs["weight_decay"], configs["batch_size"])
+            else:
+                summarizer_model = SummaryNetwork(configs["weight_decay"], configs["batch_size"], z_dim=configs["noise_dim"])
+
             train_dataset, validation_dataset = \
-                summarizer.dataset.create_summary_dataset(configs["trailer_dir"], configs["poster_dir"], batch_size)
+                summarizer.dataset.create_summary_dataset(configs["trailer_dir"], configs["poster_dir"],
+                                                          configs["batch_size"])
             if args.mock:
                 summarizer_loss = summarizer.eval.summarizer_loss_mock(beta, generator_predict)
                 discriminator_loss = summarizer.eval.discriminator_loss_mock(alpha, discriminator_predict, generator_predict)
                 combined_loss = summarizer.eval.combined_loss_mock(alpha, beta, gamma, generator_predict,
                                                               discriminator_predict, 12)
+                cap = summarizer.network.make_mock_cap()
             else:
-                summarizer_loss = summarizer.eval.summarizer_loss(beta, generator_predict)
-                combined_loss = summarizer.eval.combined_loss(alpha, beta, gamma, generator_predict,
-                                                                   discriminator_predict, 12)
+                summarizer_loss = summarizer.eval.summarizer_loss(beta)
+                combined_loss = summarizer.eval.combined_loss(alpha, beta, gamma, discriminator_predict, 12)
+                cap = summarizer.network.make_gan_cap(generator_predict)
+
+            input = tf_v1.keras.layers.Input(shape=(20, 64, 64, 3))
+            sm_output = summarizer_model(input)
+            output = cap(sm_output)
+
+            model = tf_v1.keras.Model(inputs=input, outputs=output)
 
             color_loss = summarizer.eval.color_loss(gamma, 12)
-            model = SummaryNetwork(configs["weight_decay"])
             model.compile(optimizer=tf_v1.keras.optimizers.Adam(learning_rate=configs["learning_rate"]),
-                          loss='mse', metrics=[summarizer_loss, color_loss, 'mse'])
+                          loss=combined_loss, metrics=[summarizer_loss, color_loss],
+                          experimental_run_tf_function=False)
             checkpoint = tf_v1.keras.callbacks.ModelCheckpoint(os.path.join(save_dir, "model"), monitor='val_loss',
                                                                save_best_only=True, verbose=1, mode='min')
-            callbacks_list = [checkpoint]
+            callbacks_list = [] #[checkpoint]
             training_history = model.fit(train_dataset, epochs=configs["epochs"], verbose=2,
-                      validation_data=validation_dataset, callbacks=callbacks_list)
-            model.save(os.path.join(configs["storage_dir"], args.run_name, "model"), save_format='tf')
+                       validation_data=validation_dataset, callbacks=callbacks_list)
+            summarizer_model.save(os.path.join(configs["storage_dir"], args.run_name, "model"), save_format='tf')
             summarizer.eval.show_training_plot(training_history, results_dir)
             print("Model finished training.")
+
 
 def eval_model(configs):
     run_dir = os.path.join(configs["storage_dir"], args.run_name)
     results_dir = os.path.join(run_dir, "results")
     save_dir = os.path.join(run_dir, "model")
-    model = tf_v2.saved_model.load(save_dir)
-    generator_path = configs["generator_path"]
-    if args.mock:
-        generator_path = os.path.join(generator_path, "mock")
-
-
-    generator = tf_v2.saved_model.load(export_dir=os.path.join(generator_path))
 
     # Show real poster vs predictions
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
+    generator_path = configs["generator_path"]
     if args.mock:
-        summarizer.eval.show_poster_mock_predict_comparison(model, generator, results_dir, configs["trailer_dir"], configs["poster_dir"])
-    else:
+        generator_path = os.path.join(generator_path, "mock")
+    generator = tf_v2.saved_model.load(export_dir=generator_path)
+    generator_predict = generator.signatures["serving_default"]
 
-        generator_predict = generator.signatures["serving_default"]
-        summarizer.eval.show_poster_predict_comparison(model, generator_predict, results_dir, configs["trailer_dir"], configs["poster_dir"])
+    summarizer_model = tf_v2.saved_model.load(save_dir)
+    if args.mock:
+        cap = summarizer.network.make_mock_cap()
+    else:
+        cap = summarizer.network.make_gan_cap(generator_predict)
+
+    summarizer_predict = summarizer_model.signatures["serving_default"]
+    summarizer.eval.show_poster_predict_comparison(summarizer_predict, cap , results_dir, configs["trailer_dir"], configs["poster_dir"])
     # Other evaluation metrics
 
 def main():
